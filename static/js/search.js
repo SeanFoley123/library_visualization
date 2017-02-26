99280842a6f1a0676e231ca1a0f096b2a591b861
SeanFoley123 <sean.foley@students.olin.edu>
2017-02-25 20:55:16 -0500

    Fixed spacing

--- static/js/search.js
+++ static/js/search.js
@@ -11,7 +11,7 @@ $(function() {
 	var loadingbar = svg.append("g")
 	var spacing = 40;
 	var radius = 10;
-
+	highLevel.attr("transform", "translate("+ 0+","+ height/2 +")")
 	function constructTree(levelData) {
 		if (typeof levelData == "number") {
 			console.log(levelData);
@@ -23,12 +23,11 @@ $(function() {
 		        .data(Object.keys(levelData))
 		        .enter().append("circle")
 		        .attr( "class", "forrest" )
-		        .attr( "cx", function(d, i) { return i * spacing })
-		        .attr( "r", function(d) { return radius })
+		        .attr( "cx", function(d, i) {return (width/2 - (spacing*(Object.keys(levelData).length-1)/2)) + spacing*i})
+		        .attr( "r", radius)
 		        .on("mouseover", function(d){
 		            d3.select(this).attr("fill","red");
 		            $('#title').text(d).show();
-		            console.log($("#title").width());
 		            $('#title').css("margin-left", "-" + $("#title").width()/2 + "px" );
 		        })
 		        .on("mouseout", function(d){
@@ -39,27 +38,27 @@ $(function() {
 		        	var current_selection = this;
 		        	var current_index = i;
 		        	radius = radius*2;
+		        	spacing = radius*4
 		        	highLevel.selectAll('.forrest').transition().duration(2000).attr("cx", function(d, i){
 		        		if (this != current_selection) {
 		        			return (current_index > i) ? -100:width+100;
 		        		}
 		        		else{
-		        			return width/2-radius/2;
+		        			return width/2;
 		        		}
 		        	}).attr('r', radius);
-		        	
 
-		        	// highLevel.selectAll('.forrest').filter(function () {return(this != current_selection)}).remove();	
-		        		        	// constructTree(levelData[key])
+		        	// constructTree(levelData[key]);
 		        });
 
-		    var widthAdjusted = (window.innerWidth/2 - (spacing * (Object.keys(levelData).length)/2) + radius*2);
-		    highLevel.attr("transform", "translate("+ widthAdjusted +","+ window.innerHeight/2 +")")
+		    // var widthAdjusted = (window.innerWidth/2 - (spacing * (Object.keys(levelData).length)/2) + radius*2);
 		}
 	}
 
 	$('form#search').submit(function(event) {
 		event.preventDefault();
+		radius = 10;
+		spacing = 40
 		var query = $(this).find('input[name="query"]').val();
 		loadingbar.append("rect").attr("width", 140).attr("height", 10).attr("class", "bar").attr("color", "green");
 		loadingbar.attr("transform", "translate(" + (width/2-70) + "," + 70 + ")");
